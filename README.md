cb1603323e3bfa5a7bae8836a334c7e8dcc9aa74
Aviv Ron <aviv1ron1@gmail.com>
2015-12-30 12:13:39 +0200

    initial upload

---
+++ README.md
@@ -0,0 +1,137 @@
+# arff-utils
+
+a package for creating [arff](https://weka.wikispaces.com/ARFF) files for use in [WEKA machine learning](https://weka.wikispaces.com/)
+
+# usage
+
+```javascript
+var Arff = require('arff-utils');
+var arff = new Arff.ArffWriter("relation name");
+arff.addNumericAttribute("num");
+arff.addStringAttribute("str");
+arff.addNominalAttribute("nom", ["a", "b", "c"]);
+arff.addDateAttribute("dt");
+arff.addData([1,"s","a","2001-04-03 12:12:12"]);
+arff.writeToStream(process.stdout);
+```
+# ctor
+```new Arff.ArffWriter(relation[, mode][, options])```
+* `relation` relation name
+* `mode` [optional] if entering data manually with addData one must state the type of data that will be added. The type can be of [arff.MODE_OBJECT, arff.MODE_ARRAY, arff.MODE_CSV]. default value is MODE_ARRAY. see below in [examples](#examples) for usage of each one.
+* `options` [optional]
+  * `reduce` an object mapping nominal values that should be transformed to a different value. For example `{ "false":"f" }` will transform all the nominal values "false" to "f" to instead
+  * `skipRedundantNominal` boolean whether to skip nominal attributes with only a single value. If true these will be omitted from the final output. default is false.
+
+# adding attributes
+
+all attribute adding functions receive a string as the first parameter - the name of the attribute.
+
+# addNumericAttribute
+```javascript
+arff.addNumericAttribute(name);
+```
+Add a new numeric attribute. This attribute can be of real or integer - it doesnt matter.
+
+# addStringAttribute
+```arff.addStringAttribute(name);```
+Add a new string attribute.
+
+# addNominalAttribute
+```arff.addNominalAttribute(name, values);```
+Adds a new nominal attribute. Receives an array of string values for this attribute as the second parameter.
+
+# addDateAttribute
+```arff.addDateAttribute(name[, format]);```
+Adds a new date attribute. Receives an optional second parameter of type string as the date format as stated by the [arff specs](https://weka.wikispaces.com/ARFF).
+
+# addData(data)
+`data` should be in the format as specified in the constructor.
+* for `MODE_ARRAY` (default) `data` should be an array exactly the size of the number of attributes
+* for mode `MODE_OBJECT` `data` should be an object where the property keys are the attribute names and they must match the number of attributes exactly
+* for mode `MODE_CSV` `data` should be a comma delimited string with the values matching exactly the number of attributes.
+
+After addData is called for the first time you can not add any new attributes.
+
+# writeToStream(stream)
+writes the arff content into the supplied stream
+
+# parseCsv(in, out[, options])
+Given the data in csv format `parseCsv` will iterate thru the csv and output an arff file.
+
+* `in` - filename or input stream to read csv from
+* `out` - stream to write output arff
+* `options` - [optional]
+  * `start` - what line to start reading the input csv from (default is zero)
+  * `count` - how many lines to read from the input csv (default is until the end)
+  * `autoNominals` - boolean. If true nominal values will be automatically determined from the set of available values in the data. default is false.
+
+# examples
+```
+var arff = new Arff.ArffWriter("myarff", Arff.MODE_OBJECT);
+arff.addNumericAttribute("num");
+arff.addStringAttribute("str");
+arff.addNominalAttribute("nom", ["a", "b", "c d"]);
+arff.addDateAttribute("dt");
+arff.addDateAttribute("dtf", "yy mm dd");
+
+arff.addData({
+    num: 1,
+    str: "s",
+    nom: "a",
+    dt: "2001-04-03 12:12:12",
+    dtf: "2001-04-03 12:12:12"
+});
+arff.addData({
+    num: 2,
+    str: "s2",
+    nom: "c d",
+    dt: "2015-04-03 12:12:12",
+    dtf: "2001-04-03 12:12:12"
+});
+
+arff.writeToStream(process.stdout);
+```
+```
+var arff = new Arff.ArffWriter("myarff", Arff.MODE_ARRAY);
+arff.addNumericAttribute("num");
+arff.addStringAttribute("str");
+arff.addNominalAttribute("nom", ["a", "b", "c d"]);
+arff.addDateAttribute("dt");
+arff.addDateAttribute("dtf", "yy mm dd");
+
+arff.addData([1, "s", "a", "2001-04-03 12:12:12", "2001-04-03 12:12:12"]);
+arff.addData([2, "s s", "c d", "2009-04-12 12:12:12", "2001-04-03 12:12:12"]);
+
+arff.writeToStream(process.stdout);
+```
+```
+var arff = new Arff.ArffWriter("myarff", Arff.MODE_CSV, {     
+	reduce: {
+        "c d": "c" 
+    }
+});
+arff.addNumericAttribute("num");
+arff.addStringAttribute("str");
+arff.addNominalAttribute("nom", ["a", "b", "c d"]);
+arff.addDateAttribute("dt");
+arff.addDateAttribute("dtf", "yy mm dd");
+
+arff.addData('1,s,a,"2001-04-03 12:12:12","2001-04-03 12:12:12"');
+arff.addData('2,"s s","c d","2011-04-03 12:12:12","2001-04-03 12:12:12"');
+
+arff.writeToStream(process.stdout);
+```
+```
+var arff = new Arff.ArffWriter("parsearf");
+arff.addNumericAttribute("num");
+arff.addStringAttribute("str");
+arff.addNominalAttribute("nom");
+arff.addDateAttribute("dt");
+
+//read lines 1 - 4 from test.csv and auto insert nominal values according to data:
+arff.parseCsv('test.csv', process.stdout, {
+    autoNominals: true,
+    start: 1,
+    count: 3
+});
+```
---
+++ index.js
@@ -0,0 +1,416 @@
+var _ = require("underscore");
+var util = require('util');
+var fs = require('fs');
+var Csvly = require('csvly');
+var Readable = require('stream').Readable;
+
+const ATTRIBUTE = "@ATTRIBUTE";
+const DATA = "@DATA";
+const EOL = require("os").EOL;
+
+var len = function(obj) {
+    return Object.keys(obj).length;
+}
+
+var dataTypes = {
+    INT: "integer",
+    REAL: "real",
+    NUMERIC: "numeric",
+    STRING: "string",
+    DATE: "date",
+    NOMINAL: "nominal"
+}
+
+function arrayToStream(arr, opts) {
+    Readable.call(this, {
+        objectMode: true
+    });
+    if (opts && opts.delim) {
+        this.delim = opts.delim;
+    }
+    this.arr = arr;
+    this.idx = 0;
+}
+
+util.inherits(arrayToStream, Readable);
+
+arrayToStream.prototype._read = function() {
+    if (this.idx < this.arr.length) {
+        var str = this.arr[this.idx++];
+        if (this.delim) {
+            str += this.delim;
+        }
+        this.push(str);
+    } else {
+        this.push(null);
+    }
+};
+
+var isDefined = function(obj) {
+    return !util.isNullOrUndefined(obj);
+}
+
+var MODES = {
+    CSV: 0,
+    ARRAY: 1,
+    OBJECT: 2
+}
+
+function writer(sw) {
+    this.write = function() {
+        var w = function(val, space) {
+            if (!util.isNullOrUndefined(val)) {
+                sw.write(val);
+                if (space) {
+                    sw.write(" ");
+                }
+            }
+        }
+        for (var i = 0; i < arguments.length - 1; i++) {
+            w(arguments[i], true);
+        }
+        w(arguments[arguments.length - 1]);
+    }
+    this.writeLine = function() {
+        arguments[arguments.length] = EOL;
+        arguments.length++;
+        this.write.apply(this, arguments);
+    }
+}
+
+function arff(relation, mode, options) {
+    this.relation = relation;
+    this.attributesMap = {};
+    this.attributesArr = [];
+    this.data = [];
+    this.headerMode = true;
+    this.mode = MODES.ARRAY;
+
+    // var mode;
+    // var options;
+    if (isDefined(arguments[1])) {
+        if (typeof arguments[1] == "string") {
+            mode = arguments[1];
+            if (isDefined(arguments[2])) {
+                options = arguments[2];
+            }
+        } else {
+            options = arguments[1];
+            mode = undefined;
+        }
+    }
+
+    if (isDefined(mode)) {
+        if (isDefined(MODES[mode])) {
+            this.mode = MODES[mode];
+        } else {
+            throw new Error("illegal mode value");
+        }
+    }
+    this.setMode(this.mode);
+    this.handler;
+    this.writer;
+    var self = this;
+    this.attributeHandlers = {
+        "nominal": self.addNominalAttribute,
+        "string": self.addStringAttribute,
+        "numeric": self.addNumericAttribute,
+        "real": self.addNumericAttribute,
+        "integer": self.addNumericAttribute,
+        "date": self.addDateAttribute
+    }
+    if (isDefined(options)) {
+        if (isDefined(options.reduce)) {
+            this.reduce = options.reduce;
+        }
+        if (isDefined(options.skipRedundantNominal)) {
+            this.skipRedundantNominal = options.skipRedundantNominal;
+        }
+    }
+}
+
+arff.prototype.setMode = function(mode) {
+    var self = this;
+    var handle = function(d, getLength, getArr, getValue) {
+        if (getLength(d) != self.attributesArr.length) {
+            throw new Error("incompatible data size unequal to the number of attributes");
+        }
+        self.writer.writeLine.apply(self.writer, _.map(getArr(d), function(val, index) {
+            if (isDefined(self.attributesArr[index])) {
+                var ch = ",";
+                if (index == self.lastIndex) {
+                    ch = "";
+                }
+                var value = getValue(d, val, index);
+                if (isDefined(self.reduce) && isDefined(self.reduce[value])) {
+                    value = self.reduce[value];
+                }
+                switch (self.attributesArr[index].data.type) {
+                    case dataTypes.DATE:
+                        return escape(value, '"', /./) + ch;
+                    default:
+                        return escape(value) + ch;
+                }
+            }
+        }));
+    }
+    var handleArray = function(d) {
+        handle(d, function(d) {
+                return d.length
+            }, function(d) {
+                return d
+            },
+            function(d, val, index) {
+                return val;
+            })
+    }
+    var handleObject = function(d) {
+        handle(d, function(d) {
+            return len(d)
+        }, function(d) {
+            return _.keys(d)
+        }, function(d, val, index) {
+            return d[val]
+        })
+    }
+    switch (mode) {
+        case MODES.CSV:
+            this.handler = handleArray;
+            break;
+        case MODES.OBJECT:
+            this.handler = handleObject;
+            break;
+        case MODES.ARRAY:
+            this.handler = handleArray;
+            break;
+    }
+};
+
+
+
+arff.prototype.addAttribute = function(attribute, type, meta) {
+    this.attributeHandlers[type.toLowerCase()].apply(this, [attribute, meta]);
+}
+
+arff.prototype.addNumericAttribute = function(name) {
+    this._addAttribute(name, {
+        type: dataTypes.NUMERIC
+    });
+};
+
+arff.prototype.addStringAttribute = function(name) {
+    this._addAttribute(name, {
+        type: dataTypes.STRING
+    });
+};
+
+arff.prototype.addDateAttribute = function(name, format) {
+    this._addAttribute(name, {
+        type: dataTypes.DATE,
+        format: format
+    });
+};
+
+arff.prototype.addNominalAttribute = function(name, values) {
+    if (util.isNullOrUndefined(values)) {
+        values = [];
+    }
+    var self = this;
+    if (this.reduce) {
+        values = _.map(values, function(val, index) {
+            if (isDefined(self.reduce[val])) {
+                return self.reduce[val];
+            } else {
+                return val;
+            }
+        });
+    }
+    this._addAttribute(name, {
+        type: dataTypes.NOMINAL,
+        values: values,
+        nominals: {}
+    });
+};
+
+arff.prototype._addAttribute = function(name, dataType) {
+    if (!this.headerMode) {
+        throw new Error("cannot add more attributes after adding data");
+    }
+    this.attributesMap[name] = dataType;
+    this.attributesArr.push({
+        name: name,
+        data: dataType
+    });
+};
+
+arff.prototype.scanForRedundantNominals = function() {
+    var self = this;
+    if (this.skipRedundantNominal) {
+        _.each(this.attributesArr, function(val, index) {
+            if (val.data.type == dataTypes.NOMINAL && val.data.values.length < 2) {
+                self.attributesArr[index] = undefined;
+            } else {
+                self.lastIndex = index;
+            }
+        });
+        if (util.isNullOrUndefined(this.lastIndex)) {
+            throw new Error("after skipping redundant nominals no attributes are left");
+        }
+    } else {
+        this.lastIndex = this.attributesArr.length - 1;
+    }
+
+};
+
+arff.prototype.addData = function(data) {
+    if (this.headerMode) {
+        this.headerMode = false;
+        this.scanForRedundantNominals();
+    }
+    this.data.push(data);
+};
+
+function escape(str, ch, rgx) {
+    if (util.isNullOrUndefined(str)) {
+        return "";
+    }
+    if (_.isNumber(str)) {
+        str = str.toString();
+    }
+    if (util.isNullOrUndefined(ch)) {
+        ch = "'";
+    }
+    if (util.isNullOrUndefined(rgx)) {
+        rgx = /\s/g;
+    }
+    if (rgx.test(str)) {
+        return ch + str + ch;
+    }
+    return str;
+}
+
+
+
+arff.prototype.writeHeaderToStream = function() {
+    this.writer.writeLine("@RELATION", escape(this.relation));
+    this.writer.writeLine();
+    var self = this;
+    this.attributesArr.forEach(function(att) {
+        if (isDefined(att)) {
+            switch (att.data.type) {
+                case dataTypes.DATE:
+                    self.writer.writeLine(ATTRIBUTE, escape(att.name), att.data.type, escape(att.data.format, '"', /./));
+                    break;
+                case dataTypes.NOMINAL:
+                    var attVals = _.reduce(att.data.values, function(memo, v) {
+                        return memo + escape(v) + ",";
+                    }, "{");
+                    attVals = attVals.substr(0, attVals.length - 1) + "}";
+                    self.writer.writeLine(ATTRIBUTE, escape(att.name), attVals);
+                    break;
+                default:
+                    self.writer.writeLine(ATTRIBUTE, escape(att.name), att.data.type);
+                    break;
+            }
+        }
+    });
+    self.writer.writeLine();
+    self.writer.writeLine(DATA);
+};
+
+arff.prototype.writeToStream = function(strm) {
+    this.writer = new writer(strm);
+    this.writeHeaderToStream();
+    switch (this.mode) {
+        case MODES.CSV:
+            var readable = new arrayToStream(this.data, {
+                delim: EOL
+            });
+            var csvReader = new Csvly(readable, {
+                array: true
+            });
+            csvReader.on('line', this.handler);
+            csvReader.read();
+            break;
+        default:
+            this.data.forEach(this.handler);
+            break;
+
+    }
+}
+
+arff.prototype.writeToFile = function(p) {
+    this.writeToStream(fs.createWriteStream(p));
+};
+
+arff.prototype.parseCsv = function(input, output, options) {
+    var self = this;
+    this.parseMode = true;
+    this.setMode(MODES.ARRAY);
+    var start = 0;
+    var count = undefined;
+    var autoNominals = false;
+    this.writer = new writer(output);
+
+    if (isDefined(options)) {
+        if (isDefined(options.start)) {
+            start = options.start;
+        }
+        if (isDefined(options.count)) {
+            count = options.count;
+        }
+        if (isDefined(options.autoNominals)) {
+            autoNominals = options.autoNominals;
+        }
+    }
+    var opts = {
+        array: true
+    };
+
+    var readData = function() {
+        self.scanForRedundantNominals();
+        self.writeHeaderToStream();
+        reader = new Csvly(input, opts);
+        reader.on('line', self.handler);
+        reader.read(start, count);
+    }
+
+    if (autoNominals) {
+        reader = new Csvly(input, opts);
+        reader.on('line', function(data) {
+            for (var i = 0; i < data.length; i++) {
+                if (self.attributesArr[i].data.type == dataTypes.NOMINAL) {
+                    if (util.isNullOrUndefined(self.attributesArr[i].data.nominals[data[i]])) {
+                        self.attributesArr[i].data.nominals[data[i]] = 1;
+                    }
+                }
+            }
+        });
+        reader.on('end', function() {
+            for (var i = 0; i < self.attributesArr.length; i++) {
+                self.attributesArr[i].data.values = [];
+                if (self.attributesArr[i].data.type == dataTypes.NOMINAL) {
+                    Object.keys(self.attributesArr[i].data.nominals).forEach(function(v) {
+                        if (isDefined(self.reduce)) {
+                            if (isDefined(self.reduce[v])) {
+                                v = self.reduce[v];
+                            }
+                        }
+                        self.attributesArr[i].data.values.push(v);
+                    });
+                }
+            }
+            readData();
+        });
+        reader.read(start, count);
+    } else {
+        readData();
+    }
+}
+
+module.exports = {
+    ArffWriter: arff,
+    MODE_OBJECT: "OBJECT",
+    MODE_ARRAY: "ARRAY",
+    MODE_CSV: "CSV"
+};
